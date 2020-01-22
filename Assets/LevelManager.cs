ab3834ec0d09ef51e8069c6cf23d2c9c47f7ad09
Yaugan <yaugansd45@gmail.com>
2020-01-22 21:08:55 +0530

    NumberQuiz

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level Load requested for: " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit!");
+        Application.Quit();
+    }
+}
---
+++ Assets/NumberQuiz.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberQuiz : MonoBehaviour
+{
+
+    int max;
+    int min;
+    int guess;
+
+    public int maxGuessesAllowed = 5;
+
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        NextGame();
+    }
+
+    // Update is called once per frame
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGame();
+    }
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGame();
+    }
+
+    void NextGame ()
+    {
+        guess = Random.Range(min, max +1);
+        text.text = guess.ToString();
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+        if(maxGuessesAllowed <= 0)
+        {
+            Application.LoadLevel("Win");
+        }
+    }
+}
